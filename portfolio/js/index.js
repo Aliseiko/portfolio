7f16a4a634e9e6fe602dbf8c1d611d1ad1c905c2
Aliseiko <aliseiko.dev@gmail.com>
2022-01-09 15:20:16 +0300

    feat: add portfolio images change

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,4 +1,6 @@
-let burgerButton = document.querySelector('.burger'),
+// --------------- open / close menu ------------------
+
+const burgerButton = document.querySelector('.burger'),
     menu = document.querySelector('.nav'),
     cover = document.querySelector('.cover');
 burgerButton.addEventListener('click', function () {
@@ -17,25 +19,22 @@ function closeMenu(event) {
     }
 }
 
-console.log('1. Вёрстка соответствует макету. Ширина экрана 768px +48\n' +
-    '        блок <header> +6\n' +
-    '        секция hero +6\n' +
-    '        секция skills +6\n' +
-    '        секция portfolio +6\n' +
-    '        секция video +6\n' +
-    '        секция price +6\n' +
-    '        секция contacts +6\n' +
-    '        блок <footer> +6\n' +
-    '2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15\n' +
-    '        нет полосы прокрутки при ширине страницы от 1440рх до 768рх +5\n' +
-    '        нет полосы прокрутки при ширине страницы от 768рх до 480рх +5\n' +
-    '        нет полосы прокрутки при ширине страницы от 480рх до 320рх +5\n' +
-    '3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22\n' +
-    '        при ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка +2\n' +
-    '        при нажатии на бургер-иконку справа плавно появляется адаптивное меню, бургер-иконка изменяется на крестик +4\n' +
-    '        высота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана +4\n' +
-    '        при нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку +4\n' +
-    '        бургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений +2\n' +
-    '        ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +2\n' +
-    '        при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку +4\n' +
-    'ИТОГО: 85 баллов')
+// --------------- change imgs on btn click in portfolio ------------------
+
+const buttonsBlock = document.querySelector('.buttons-block'),
+    portfolioButtons = document.querySelectorAll('.portfolio-button'),
+    portfolioImages = document.querySelectorAll('.portfolio-photo-item');
+
+buttonsBlock.addEventListener('click', changeImage);
+
+
+function changeImage(event) {
+    if(event.target.classList.contains('portfolio-button')) {
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg`);
+        portfolioButtons.forEach(btn => {
+            btn.classList.remove('button-active');
+        });
+        event.target.classList.add('button-active');
+    }
+}
+
